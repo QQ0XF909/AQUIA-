8e425b9d65a88ca8ae61232c5451f0ade98314bb
QQ0XF909 <qqqx9009qqq@gmail.com>
2026-08-06 02:49:32 +0300

    event refactoring patch

--- core/event/include/Event_core.hpp
+++ core/event/include/Event_core.hpp
@@ -1,4 +1,5 @@
 #pragma once 
+#include <iostream>
 #include <string>
 #include <cstdint>
 #include <chrono>
@@ -8,84 +9,56 @@
 #include <boost/uuid/uuid_generators.hpp>
 #include <boost/uuid/uuid_io.hpp>
 #include <boost/uuid/string_generator.hpp>
+/*uuid Boost  https://www.boost.org/doc/libs/1_73_0/libs/uuid/doc/uuid.html*/
 
-/* uuid Boost  https://www.boost.org/doc/libs/1_73_0/libs/uuid/doc/uuid.html */
-
-#include <stdio.h>
+#include <cstdio>
 #include <nlohmann/json.hpp>
 #include <functional>
 #include <stdexcept>
 
-namespace CORE::Domain {
+namespace CORE {
+    namespace Domain {
+        enum class EventType : std::uint8_t {
+            Unknown = 0,
+            UserLogin,
+            UserLogout,
+            UserRegistered,
+            UserAgeSent,
+            MessageDelivered,
+            MessageRead,
+            MessageDeleteOnlyForYourself,
+            DeleteForEveryone,
+            MessageDelete,
+            PostCreated,
+            PostDelete
+        };
+    
+        class Event {
+            public :
+                Event () = default;
+                explicit Event (EventType type) :
+                    EventUUID(generateUUID()), EventTYPE(type), Timing(std::chrono::system_clock::now()){
+                        if (type == EventType::Unknown){ 
+                            std::cout << "Warning : Event is unknown type!" << std::endl; 
+                        }
+                };
+                virtual ~Event() = default;
 
-    enum class EventType : std::uint8_t {
-        Unknown = 0,
-        UserLogin,
-        UserLogout,
-        UserRegistered,
-        UserAgeSent,
-        MessageDelivered,
-        MessageRead,
-        MessageDeleteOnlyForYourself,
-        DeleteForEveryone,
-        MessageDelete,
-        PostCreated,
-        PostDelete
-    };
- 
-    class Event {
-        public :
-            Event () = default;
+                const std::string& getEventID() const noexcept { return const boost::uuds::to_string(EventUUID); }
+                EventType getEventType() const noexcept { return EventTYPE; }
+                std::chrono::system_clock::time_point getOccurredAt() const noexcept { return Timing; }
 
-            explicit Event (EventType type) :
-                EventUUID(generateUUID()), EventTYPE(type), Timing(std::chrono::system_clock::now()){
-                    if (type == EventType::Unknown){ 
-                        std::cout << "Warning : Event is unknown type!" << std::endl; 
-                    }
-            };
-
-            virtual ~Event() = default;
-
-         
-            const std::string& getEventID() const noexcept { return EventUUID; }
-            EventType getEventType() const noexcept { return EventTYPE; }
-            std::chrono::system_clock::time_point getOccurredAt() const noexcept { return Timing; }
-
-           
-            virtual nlohmann::json toJson() const {
-                nlohmann::json jt;
-                jt["event_uuid"] = EventUUID;
-                jt["event_type"] = static_cast<std::uint8_t>(EventTYPE);
-                jt["event_time"] = std::chrono::duration_cast<std::chrono::milliseconds>(
-                                                                Timing.time_since_epoch()).count();
-                return jt;
-            }
-            // ============================ переписываем 
-            static std::shared_ptr<Event> FromJson(const nlohmann::json& jt){
-                if (!jt.contains("evennt_type") || !jt.contains("evennt_type")){
-                    return nullptr;
-                }
-                
-                auto eve = std::make_shared<Event>();
-                eve->EventUUID = jt.["event_uuid"].get<std::string>();
-                eve->EventTYPE = static_cast<EventType>(jt["event_type"].get<std::uint8_t>());
-                if (jt.contains("event_time")){
-                    auto ms = jt["event_time"].get<std::uint64_t>();
-                    eve->Timing = std::chrono::system_clock::time_point(std::chrono::milliseconds(ms));                  
-                }
-            }
-          // ============================
-        protected:
-            Event(const Event&) =  default;
-            Event(Event&&) = default;
-        private:
-            boost::uuids::random_genrator generateUUID () {};
-            
-            EventType EventTYPE = EventType::Unknown;
-            std::chrono::system_clock::time_point Timing;
-
-            boost::uuids::uuid EventUUID; 
-            /* std::string USER_LOGIN; */
-            
-    };
-} // namespace CORE::Domain 
+                virtual nlohmann::json toJson() const ;
+                static std::shared_ptr<Event> FromJson(const nlohmann::json& jt);
+            protected:
+                Event(const Event&) =  default;
+                Event(Event&&) = default;
+            private:
+                boost::uuids::random_genrator generateUUID ();
+                EventType EventTYPE = EventType::Unknown;
+                std::chrono::system_clock::time_point Timing;
+                boost::uuids::uuid EventUUID; 
+                /* std::string USER_LOGIN; */
+        };
+    }  /* namespace Domain */
+} /* namespace CORE */
--- core/event/src/Event_core.cpp
+++ core/event/src/Event_core.cpp
@@ -1,6 +1,4 @@
-#pragma once 
-#define BOOST_UUID_USE_SSE2
- //  add_compile_definitions(BOOST_UUID_USE_SSE2)
+#define BOOST_UUID_USE_SSE2 //  add_compile_definitions(BOOST_UUID_USE_SSE2)
 #include "../include/Event_core.hpp"
 
 
@@ -13,27 +11,34 @@ namespace CORE {
             return GenUUID;
         };
         
-        virtual nlohmann::json Event::toJson() const {
-            /*code*/
-            return jt;
+        nlohmann::json Event::toJson() const {
+            nlohmann::json js;
+            js["event_uuid"] = boost::uuids::to_string(EventUUID);
+            js["event_type"] = static_cast<uint8_t>(EventTYPE);
+            js["event_time"] = std::chrono::duration_cast<std::chrono::milliseconds>(Timing.time_since_epoch()).count();
+            return js;
         }
 
-        static std::shared_ptr<Event> Event::FromJson (const nlohmann::json& js) { 
+        std::shared_ptr<Event> Event::FromJson (const nlohmann::json& js) { 
             try {
-                if (!js.contains("event_type") || !js.contains("event_uuid")) {
-                    throw std::runtime_error("Event keys and arguments were not found in the file!")
-                    // return nullptr;
+                if (!js.contains("event_type") || !js.contains("event_uuid") || !js.contains("event_time")) {
+                    throw std::runtime_error("Event keys and arguments were not found in the file!");
                 }    
+                
                 auto _event = std::make_shared<Event>();
                 boost::uuids::string_generator str_gen;
-                
+
                 _event -> EventUUID = str_gen(js["event_uuid"].get<std::string>());
                 std::uint8_t type_value = js["event_type"].get<std::uint8_t>(); 
-                if (type_value > static_cast<std::uint8_t>(EventTYPE::PostDelete)) {
+                
+                if (type_value > static_cast<std::uint8_t>(EventType::PostDelete) || type_value == static_cast<std::uint8_t>(EventType::Unknown)) {
                     throw std::runtime_error ("Invalid event type!");
                 }
                 _event -> EventTYPE = static_cast<EventType>(type_value);
-                
+                auto ms = js["event_time"].get<std::uint64_t>();
+                _event -> Timing = std::chrono::system_clock::time_point(std::chrono::milliseconds(ms));
+
+                return _event;
 
             } catch(const std::exception& exc) {
                 std::cerr << "Error: " << exc.what() << '\n';
